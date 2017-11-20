6d402960c79db7fbb817ba777c6322ae782db8df
s3ud-alshaman <alshaman64@gmail.com>
2017-11-20 12:21:24 +0300

    Update saud2.js

--- saud2.js
+++ saud2.js
@@ -1,7 +1,7 @@
 {
   "actors": [
     {
-      "name": "Tmam",
+      "name": "ToTa",
       "description": "William Bradley 'Brad' Pitt is an American actor and film producer. He has received a Golden Globe Award, a Screen Actors Guild Award, and three Academy Award nominations in acting categories",
       "dob": "050093882",
        "phone": "Temoo",
