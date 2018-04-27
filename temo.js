0de7cc57d63623177659246f51aa1fbf35d00fde
s3ud-alshaman <alshaman64@gmail.com>
2018-04-27 22:38:01 +0300

    help

--- temo.js
+++ temo.js
@@ -8,6 +8,15 @@
     "email": "alshaman64@gmail.com",
     "location": "المبنى الرئيسي - الدور الأرضي",
     "img": "/apps/dota2/images/heroes/antimage_full.png?",
+  },
+  {
+    "id": 2,
+    "name": "مساعد المستشفى",
+    "ext": "9090",
+    "more_ext": "4567",
+    "phone":"0144245814",
+    "email": "alshaman64@gmail.com",
+    "location": "المبنى الرئيسي - الدور الثاني",
+    "img": "/apps/dota2/images/heroes/antimage_full.png?",
   }
-
 ]
