dc9815d9017f677e809e45f7ea77a527552e9fff
s3ud-alshaman <alshaman64@gmail.com>
2018-05-02 19:38:16 +0300

    tot

--- temo.js
+++ temo.js
@@ -2,7 +2,7 @@
   {
     "id": 1,
     "name": "مدير المستشفى",
-    "name_en": "saud",
+    "name_en": "Hospital Manager",
     "ext": "9090",
     "more_ext": "9091",
     "phone": "0144245814",
@@ -14,14 +14,14 @@
   },
   {
     "id": 2,
-    "name": "مساعد المستشفى",
-    "name_en": "temo",
-    "ext": "9090",
-    "more_ext": "4567",
+    "name": "السنترال",
+    "name_en": "Central",
+    "ext": "7070",
+    "more_ext": "8",
     "phone": "0144245814",
     "email": "alshaman64@gmail.com",
-    "location": "المبنى الرئيسي - الدور الثاني",
-    "location_en": "Main Building - Ground Floor",
+    "location": "المبنى الرئيسي - البدروم",
+    "location_en": "Main Building - Basement",
     "img": "/apps/dota2/images/heroes/antimage_full.png?",
     
   }
