0919381a9db08f1e3492978f4cc2dcfe704bf792
s3ud-alshaman <alshaman64@gmail.com>
2018-05-13 22:56:32 +0300

    22222

--- Tbl.js
+++ Tbl.js
@@ -1,28 +1,12 @@
 [
   {
-    "id": 1,
     "name": "مدير المستشفى",
     "name_en": "Hospital Director",
-    "ext": "9090",
-    "more_ext": "9091 - 9007",
-    "phone": "0144259090",
-    "email": "",
-    "location": "المبنى الرئيسي - الدور الأرضي",
-    "location_en": "Main Building - Ground Floor",
-    "img": "/",
     
   },
   {
-    "id": 2,
     "name": "السنترال",
     "name_en": "Central",
-    "ext": "8",
-    "more_ext": "7070",
-    "phone": "0144259090",
-    "email": "",
-    "location": "المبنى الرئيسي - القبو",
-    "location_en": "Main Building - Basement",
-    "img": "/Central.png",
     
   }
 ]  
