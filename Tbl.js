28519c797f53da7c134e385cecdccc69c5da7f3b
s3ud-alshaman <alshaman64@gmail.com>
2018-05-15 04:15:34 +0300

    Test til 2

--- Tbl.js
+++ Tbl.js
@@ -1,20 +1,20 @@
 [
   {
     "name": "مدير المستشفى",
+    "name_en": "hospital manager",
     "urlPdf": "https://github.com/s3ud-alshaman/took/raw/master/PLASTIC%20SURGERY.pdf",
-    "name_en": "",
     
   },
   {
     "name": "السنترال",
+    "name_en": "Central",
     "urlPdf": "https://github.com/s3ud-alshaman/took/raw/master/test2.pdf",
-    "name_en": "",
     
   },
   {
-    "name": "هنو",
-    "urlPdf": "https://developer.apple.com/programs/terms/apple_developer_agreement.pdf",
+    "name": "Test",
     "name_en": "",
+    "urlPdf": "https://developer.apple.com/programs/terms/apple_developer_agreement.pdf",
     
   }
 ]  
