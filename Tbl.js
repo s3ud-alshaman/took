86a270dea68b61ec3aad4d0562a59028b82ca61a
s3ud-alshaman <alshaman64@gmail.com>
2018-05-19 07:39:11 +0300

    التخدير و اخصائي الجراحة العامة في الطوارئ

--- Tbl.js
+++ Tbl.js
@@ -1,20 +1,15 @@
 [
   {
-    "name": "مدير المستشفى",
-    "name_en": "hospital manager",
-    "urlPdf": "https://github.com/s3ud-alshaman/took/raw/master/PLASTIC%20SURGERY.pdf",
+    "name": "التخدير",
+    "name_en": "Anesthesia",
+    "urlPdf": "https://github.com/s3ud-alshaman/took/raw/master/ANAESTHESI.pdf",
     
   },
   {
-    "name": "السنترال",
-    "name_en": "Central",
-    "urlPdf": "https://github.com/s3ud-alshaman/took/raw/master/test2.pdf",
-    
-  },
-  {
-    "name": "تجربة",
-    "name_en": "Test",
-    "urlPdf": "https://developer.apple.com/programs/terms/apple_developer_agreement.pdf",
+    "name": "اخصائي الجراحة العامة - الطوارى",
+    "name_en": "E.R GENERAL SURGERY SPECIALISTS ",
+    "urlPdf": "https://github.com/s3ud-alshaman/took/raw/master/ER%20SPECIALIST.pdf",
     
   }
+
 ]  
