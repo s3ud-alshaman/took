d15654dc6de001386763fbe814b4241838a373a5
s3ud-alshaman <alshaman64@gmail.com>
2018-05-21 07:05:02 +0300

    أطباء الأنف و الإذن و الحنجرة

--- Tbl.js
+++ Tbl.js
@@ -17,6 +17,13 @@
     "urlPdf": "https://github.com/s3ud-alshaman/took/raw/master/UROLOGY.pdf",
     
   },
+  },
+    {
+    "name": "أطباء الأنف و الإذن و الحنجرة",
+    "name_en": "ENT DUTY ROASTER",
+    "urlPdf": "https://github.com/s3ud-alshaman/took/raw/master/ENT_DUTY_ROASTER%20.pdf",
+    
+  },
  {
     "name": "أطباء الباطنية",
     "name_en": "Internal Medicine",
