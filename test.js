96385dc0b4eb1342f97c7fe245daa312d69d9c9b
Saud Al.shaman <alshaman64@gmail.com>
2018-06-13 00:05:12 +0300

    Yelp

--- test.js
+++ test.js
@@ -1,18 +1,19 @@
-‪[‬
-‪  {‬
-‪    "id": 2,
-‪    "name": "د/ فهد",‬
-‪    "name_en": "Dr/ Fahd",‬
-‪    "tbl": "عيون",‬
-‪    "tbl_en": "",‬
-‪    "Month": "8",‬‪    ‬
-‪  },‬
-‪  {‬
-‪    "id": 2,‬
-‪    "name": "د/ فهد",‬
-‪    "name_en": "Dr/ Fahd",‬
-‪    "tbl": "عيون",‬
-‪    "tbl_en": "",‬
-‪    "Month": "8",‬ ‬
-‪  }‬
-‪]‬
+[
+{
+id: 1,
+name: "Instagram Firebase",
+link: "https://www.letsbuildthatapp.com/course/instagram-firebase",
+imageUrl: "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/04782e30-d72a-4917-9d7a-c862226e0a93",
+number_of_lessons: 49
+},
+{
+id: 4,
+name: "Kindle Basic Training",
+link: "https://www.letsbuildthatapp.com/basic-training",
+imageUrl: "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/a6180731-c077-46e7-88d5-4900514e06cf",
+number_of_lessons: 19
+},
+{
+name: "Yelp"
+}
+]
